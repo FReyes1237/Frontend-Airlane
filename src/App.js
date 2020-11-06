36f6577998f0abb06bfcf7595cb6fc5abe6d5a28
Fausto Reyes <fausto.reyes@est.uca.edu.ni>
2020-11-05 19:13:48 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import react, {useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function App() {
+
+  const baseUrl = "https://localhost:44345/api/Aeronaves";
+  const [data, setData] = useState([]);
+
+  const peticionesGet = async()=>{
+    await axios.get(baseUrl)
+    .then(Response=>{
+      setData(Response.data);
+    }).catch(error=>{
+      console.error();
+    });
+  }
+
+  useEffect(()=>{
+    peticionesGet();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Aeronave ID</th>
+            <th>Modelo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(Aeronave=>(
+            <tr key={Aeronave.aeronaveID}>
+              <td>{Aeronave.aeronaveID}</td>
+              <td>{Aeronave.modelo}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
