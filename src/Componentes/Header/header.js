93235888e8867060751bae2009970657bf4d2d85
Fausto Reyes <fausto.reyes@est.uca.edu.ni>
2020-11-17 18:38:20 -0600

    Frontend casi funcional

--- src/Componentes/Header/header.js
+++ src/Componentes/Header/header.js
@@ -2,25 +2,30 @@ import React from 'react'
 import './header.css'
 import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Aeronave from '../Content/Aeronave';
 
 const header = () => {
     return (
-        <div>
-            <div className="header">
-                <Navbar bg="secondary" expand="lg">
-                    <Navbar.Brand href="#home">Aerolinea Online</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link to="/">Página Principal</Nav.Link>
-                            <Nav.Link href="#link">Compra de tickets</Nav.Link>
-                            <Nav.Link href="#link">Calendario de vuelos</Nav.Link>
-                            <Nav.Link to="Componentes/Content/Aeronave.js">Lista de Aeronaves y disponibilidad de butacas</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
+        <Router>
+            <div>
+                <div className="header">
+                    <Navbar bg="secondary" expand="lg">
+                        <Navbar.Brand href="#home">Aerolinea Online</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Nav.Link to="/">Página Principal</Nav.Link>
+                                <Nav.Link to="/ct">Compra de tickets</Nav.Link>
+                                <Nav.Link to="/calendar">Calendario de vuelos</Nav.Link>
+                                <Nav.Link to="/listaAero">Lista de Aeronaves y disponibilidad de butacas</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                    <Route path="/listaAero" exact component={Aeronave} />
+                </div>
             </div>
-        </div>
+        </Router>
     );
 };
 
